ac7c9ab81a81eb45d9805d40b2b7ebc10dcda36c
kdawg-codes <ksyoung@crimson.ua.edu>
2023-11-05 18:12:00 -0800

    Importy numpy and matplotlib

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+n = input("You're in the Lost Forest. Go left or right?")
+
+while n == "right":
+    n = input("You're in the Lost Forest. Go left or right?")
+print("You got out of the Lost Forest!")
+
+mysum = 0
+for i in range(7,10):
+    mysum -= i
+print(mysum)
+
+mysum = 0
+for i in range(5, 11, 2):
+    mysum += i
+    if mysum == 5:
+        break
+        mysum += 1
+print(mysum)
+

6007e1776eb0ebbc0cb1b8d7487047d87653cc7a
kdawg-codes <ksyoung@crimson.ua.edu>
2023-11-07 18:31:42 -0800

    Add pkgtest

--- main.py
+++ main.py
@@ -3,22 +3,30 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-n = input("You're in the Lost Forest. Go left or right?")
+#n = input("You're in the Lost Forest. Go left or right?")
+#
+#while n == "right":
+#    n = input("You're in the Lost Forest. Go left or right?")
+#print("You got out of the Lost Forest!")
+#
+#mysum = 0
+#for i in range(7,10):
+#    mysum -= i
+#print(mysum)
+#
+#mysum = 0
+#for i in range(5, 11, 2):
+#    mysum += i
+#    if mysum == 5:
+#        break
+#        mysum += 1
+#print(mysum)
 
-while n == "right":
-    n = input("You're in the Lost Forest. Go left or right?")
-print("You got out of the Lost Forest!")
-
-mysum = 0
-for i in range(7,10):
-    mysum -= i
-print(mysum)
-
-mysum = 0
-for i in range(5, 11, 2):
-    mysum += i
-    if mysum == 5:
-        break
-        mysum += 1
-print(mysum)
-
+class WordTrigger:
+    """
+    I don't know what I'm doing
+    """
+class TitleTrigger(WordTrigger):
+    """
+    Subclass of WordTrigger that represents a Trigger which checks if the story's title matches a given word
+    """
